592027c61b7917438537815ac002a798c8fb0947
bhumijgupta <bhumijgupta@gmail.com>
2021-02-28 11:21:47 +0530

    Add reload functionality
    Signed-off-by: bhumijgupta <bhumijgupta@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import chowkidar from "chokidar";
 import path from "path";
 import child_process from "child_process";
 import tree_kill from "tree-kill";
+import { Writable } from "stream";
 
 const spawn = child_process.spawn;
 
 class daenode {
   private processExited = false;
-  private nodeProcess!: child_process.ChildProcessByStdio<null, null, null>;
+  private nodeProcess!: child_process.ChildProcessByStdio<Writable, null, null>;
   private pathsToWatch = [
     path.join(process.cwd(), "/**/*.js"),
     path.join(process.cwd(), "/**/*.json"),
@@ -28,10 +29,26 @@ class daenode {
     this.watchFiles();
     process.once("SIGINT", async () => await this.exit("SIGINT"));
     process.once("SIGTERM", async () => await this.exit("SIGTERM"));
+    process.stdin.on("close", (had_error) => console.log("stdin close"));
+    process.stdin.on("connect", () => console.log("stdin connect"));
+    process.stdin.on("end", () => console.log("stdin end"));
+    process.stdin.on("error", (err) => console.log({ "stdin err": err }));
+    process.stdin.on("data", async (chunk) => {
+      const str = chunk.toString();
+      if (str === "rs\n") {
+        await this.reload();
+      }
+    });
+  };
+  private reload = async () => {
+    if (!this.processExited) {
+      await this.stopProcess();
+    }
+    this.nodeProcess = this.startProcess();
   };
   private exit = async (signal: string) => {
     this.print("debug", "Detected " + signal);
-    await this.stopProcess(this.nodeProcess);
+    await this.stopProcess();
     process.exit();
   };
   private watchFiles = () => {
@@ -42,9 +59,7 @@ class daenode {
       })
       .on("all", async () => {
         this.print("info", "File changed");
-        await this.stopProcess(this.nodeProcess);
-        this.nodeProcess = this.startProcess();
-        this.processExited = false;
+        await this.reload();
       });
   };
   private print = (type: keyof Console, message: string) => {
@@ -53,7 +68,7 @@ class daenode {
   private startProcess = () => {
     // Why spawn - https://stackoverflow.com/questions/48698234/node-js-spawn-vs-execute
     const nodeProcess = spawn("node", [process.argv[2], "--colors"], {
-      stdio: [process.stdin, process.stdout, process.stderr],
+      stdio: ["pipe", process.stdout, process.stderr],
     });
     // First exit happens and then close
     // Exit ->child process exits but stdio is not closed
@@ -61,6 +76,11 @@ class daenode {
     //   nodeProcess.on("exit", (code) => {
     //     this.print("log",`Process ${nodeProcess.pid} exited with code ${code}`);
     //   });
+    process.stdin.pipe(nodeProcess.stdin);
+    nodeProcess.stdin.on("close", () => {
+      process.stdin.unpipe(nodeProcess.stdin);
+      process.stdin.resume();
+    });
     nodeProcess.on("close", (code, signal) => {
       this.processExited = true;
       this.print(
@@ -78,15 +98,13 @@ class daenode {
     return nodeProcess;
   };
 
-  private stopProcess = async (
-    targetProcess: child_process.ChildProcessByStdio<null, null, null>
-  ) => {
+  private stopProcess = async () => {
     if (this.processExited) return true;
-    this.print("debug", `Stopping process ${targetProcess.pid}`);
+    this.print("debug", `Stopping process ${this.nodeProcess.pid}`);
     return new Promise<boolean>((resolve, reject) => {
-      tree_kill(targetProcess.pid, "SIGTERM", (err) => {
+      tree_kill(this.nodeProcess.pid, "SIGTERM", (err) => {
         if (err)
-          tree_kill(targetProcess.pid, "SIGKILL", () => {
+          tree_kill(this.nodeProcess.pid, "SIGKILL", () => {
             this.processExited = true;
             resolve(true);
           });
